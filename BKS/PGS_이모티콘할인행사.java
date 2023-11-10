64c1c6e986a8230083f35fb1a44fcfd001da5a3d
kyoungsungBack <5165776@naver.com>
2023-11-11 01:02:42 +0900

    20231107 / 2주차 / [PGS] 이모티콘 할인행사 / lv2

--- BKS/PGS_이모티콘할인행사.java
+++ BKS/PGS_이모티콘할인행사.java
@@ -3,64 +3,62 @@ package BKS;
 import java.util.Arrays;
 
 public class PGS_이모티콘할인행사 {
-    static int[] discount = {10, 20, 30, 40};
-    static int maxOfSubscribe;
-    static int maxOfCost;
+    static int[] discounts = {10, 20, 30, 40};
+    static int maxSubscribe = 0;
+    static int maxCost = 0;
 
     public static void main(String[] args) {
         int[] answer = new int[2];
         int[][] users = {{40, 10000}, {25, 10000}};
         int[] emoticons = {7000, 9000};
 
-        // 1. 이모티콘 플러스 서비스 가입자를 최대한 늘리는 것.
-        // 2. 이모티콘 판매액을 최대한 늘리는 것.
-        // 할인율은 10%, 20%, 30%, 40% -> 할인율 배열 {10, 20, 30, 40}
-        findResult(0,emoticons.length, new int[emoticons.length],users,emoticons);
-        answer[0] = maxOfSubscribe;
-        answer[1] = maxOfCost;
+        // 이모티콘 플러스 서비스 가입자를 최대한 늘리는 것.
+        // 이모티콘 판매액을 최대한 늘리는 것.
+        // 일정 할인율 이상이면 이모티콘 구매
+        // 할인율 적용된 이모티콘 구매액이 최대금액 넘으면 플러스 구독
+        findResult(0, new int[emoticons.length], users, emoticons);
+        answer[0] = maxSubscribe;
+        answer[1] = maxCost;
         System.out.println(Arrays.toString(answer));
     }
 
-    public static void findResult(int index,int emoticonsLength, int[] discounts,int[][] users, int[] emoticons){
-        // 모든 할인율에 대해서 계산을 한다.
-        if (index == emoticonsLength) {
-            int subscribe = 0; // 플러스 구독 수
-            int cost = 0; // 이모티콘 구매액
+    public static void findResult(int idx, int[] discount, int[][] users, int[] emoticons) {
+        if(idx == emoticons.length) { // 현재 이모티콘 개수에 대한 각 할인율 적용됐을 때
+            int userSubscribe = 0; // 현재 턴에서 사용자들의 구독 수
+            int userCost = 0; // 현재 턴에서 사용자들의 이모티콘 구매액
 
-            for (int[] user: users){
-                int userDiscountRate = user[0]; // 현재 고객의 이모티콘 구매 조건 할인율
-                int userMaxCost = user[1]; // 현재 고객의 플러스 구독에 필요한 최소금액
+            // 모든 사용자들에 대한 구독자 및 구매액 구하기
+            for(int[] user : users) {
+                int userDiscountRate = user[0];
+                int userPurchaseCost = user[1];
+                int totalPurchase = 0;
 
-                int sum = 0; // 고객마다 할인된 이모티콘 구매액
-
-                for (int i = 0; i < emoticons.length; i++){
-                    if (discounts[i] >= userDiscountRate){
-                        sum += (emoticons[i] / 100) * (100 - discounts[i]);
+                for(int i = 0 ; i < emoticons.length ; i++) {
+                    if(discount[i] >= userDiscountRate) { // 현재 할인율이 사용자 정의 할인율 보다 크면 구매
+                        totalPurchase += (emoticons[i] * (100 - discount[i])) / 100;
                     }
                 }
 
-                if (sum >= userMaxCost) subscribe++; // 할인된 이모티콘 구매액이 플러스 구독에 필요한 최소금액 크면 구독
-                else cost+=sum; // 아니면, 이모티콘 구매
+                // 현재 이모티콘 구매액이 사용자정의 구매액보다 같거나 크면 플러스 구독
+                if (totalPurchase >= userPurchaseCost) { // 크거나 같으면 플러스 구독
+                    userSubscribe++;
+                } else { // 작으면 그때의 구매액
+                    userCost += totalPurchase;
+                }
             }
 
-            if (subscribe > maxOfSubscribe) { // 구독자가 발생하면 구독자 수와 구매액 할당
-                maxOfSubscribe = subscribe;
-                maxOfCost = cost;
-            } else if (subscribe == maxOfSubscribe) { // 구독자가 같으면 현재 구매액만 할당
-                maxOfCost = Math.max(maxOfCost,cost);
+            if (maxSubscribe < userSubscribe) {  // 플러스 구독이 우선순위가 높으므로 구독자, 구매액 갱신
+                maxSubscribe = userSubscribe;
+                maxCost = userCost;
+            } else if (maxSubscribe == userSubscribe) { // 플러스 구독 없으므로 구매액 큰 값으로 갱신
+                maxCost = Math.max(maxCost, userCost);
             }
-            // 이때, subscribe < maxOfSubscribe 를 작성하지 않으면서 구독자와 구매액이 작은 경우를 무시하게 된다.
-
-            // 최종적으로 최대 구독자 수와 최대 구매액을 찾게 된다.
             return;
         }
 
-        // 재귀를 통해 각 이모티콘들에 대한 경우의 수를 구한다.
-        //  (최대 4^7: 4는 할인율 개수, 7는 이모티콘 최대 개수)
-        //    재귀호출이 아닐 시에 최대 4중 반복을 돌려야 한다.
-        for (int i = 0; i < 4; i++){
-            discounts[index] = discount[i];
-            findResult(index+1,emoticonsLength,discounts,users,emoticons);
+        for(int i = 0 ; i < discounts.length ; i++) {
+            discount[idx] = discounts[i];
+            findResult(idx + 1, discount, users, emoticons);
         }
     }
 }
