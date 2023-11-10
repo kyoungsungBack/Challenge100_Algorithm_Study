eccab5c4cf5a577edeb36f28130fb9247d5a3865
kyoungsungBack <5165776@naver.com>
2023-11-11 00:22:46 +0900

    20231110 / 3주차 / [PGS] 다리를 지나는 트럭 / lv2

--- BKS/PGS_다리를지나는트럭.java
+++ BKS/PGS_다리를지나는트럭.java
@@ -8,27 +8,35 @@ public class PGS_다리를지나는트럭 {
         int[] truck_weights = {7,4,5,6};
         int bridge_length = 2;
         int weight = 10;
-        Queue<Integer> queue = new LinkedList<>();
-        int weightSum = 0;
-        int answer = 0, index = 0;
 
-        for (int i = 0; i < bridge_length; i++) queue.offer(0);
+        Queue<Integer> que = new LinkedList<>();
+        int answer = 0;
+        int boardTruckSum = 0;
+        int idx = 0;
 
-        while (index < truck_weights.length) {
-            int n = queue.poll();
-            weightSum = weightSum - n;
-            if (weightSum + truck_weights[index] <= weight) {
-                weightSum += truck_weights[index];
-                queue.offer(truck_weights[index++]);
-            } else {
-                queue.offer(0);
-            }
-            answer += 1;
+        for (int i = 0 ; i < bridge_length ; i++) {
+            que.offer(0); // que에 다리 길이만큼 0을 세팅해준다.
         }
 
-        while (!queue.isEmpty()) {
-            answer += 1;
-            queue.poll();
+        while (idx < truck_weights.length) {
+            int curTruck = que.poll(); // 하차 트럭
+            boardTruckSum -= curTruck; // 현재 다리에 있는 트럭들 무게
+
+            // 대기 트럭의 탑승여부(다리를 한 칸씩 밀어준다고 생각)
+            //  마지막 트럭 탑승 시 반복문 탈출
+            if (boardTruckSum + truck_weights[idx] <= weight) { // 승차 가능
+                boardTruckSum += truck_weights[idx];
+                que.offer(truck_weights[idx++]);
+            } else { // 승차 불가능
+                que.offer(0);
+            }
+            answer++;
+        }
+
+        // 마지막 트럭 하차까지 걸리는 시간
+        while (!que.isEmpty()) {
+            answer++;
+            que.poll();
         }
 
         System.out.println(answer);
