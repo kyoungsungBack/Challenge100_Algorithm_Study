dfbfd6b2c73808bdfccc380a81c10ab10c9e2d58
kyoungsungBack <5165776@naver.com>
2023-11-21 22:31:02 +0900

    20231121 / 5주차 / [PGS] 여행경로 / lv3

---
+++ BKS/PGS_여행경로.java
@@ -0,0 +1,37 @@
+package BKS;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class PGS_여행경로 {
+    static boolean[] visited;
+    static ArrayList<String> ans;
+    public static void main(String[] args) {
+        String[][] tickets = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL", "SFO"}};
+        String[] answer = {};
+        ans = new ArrayList<>();
+        visited = new boolean[tickets.length];
+        Arrays.fill(visited, false);
+
+        dfs(0, "ICN", tickets, "ICN");
+        Collections.sort(ans);
+        answer = ans.get(0).split(" ");
+
+        System.out.println(answer);
+    }
+    static public void dfs(int cnt, String ticket, String[][] tickets, String path) {
+        if (cnt == tickets.length) {
+            ans.add(path);
+            return;
+        }
+
+        for (int i = 0 ; i < tickets.length ; i++) {
+            if (visited[i] == false && ticket.equals(tickets[i][0])) {
+                visited[i] = true;
+                dfs(cnt + 1, tickets[i][1], tickets, path + " " + tickets[i][1]);
+                visited[i] = false;
+            }
+        }
+    }
+}
+
