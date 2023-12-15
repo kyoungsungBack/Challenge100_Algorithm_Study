5f7caad349c9cf027b76da4172e5d00bcfbb9014
kyoungsungBack <5165776@naver.com>
2023-12-15 23:38:59 +0900

    20231215 / 개인풀이 / [PGS] 크레인인형뽑기게임

--- BKS/PGS_크레인인형뽑기게임.java
+++ BKS/PGS_크레인인형뽑기게임.java
@@ -3,6 +3,7 @@ package BKS;
 import java.util.Stack;
 
 public class PGS_크레인인형뽑기게임 {
+    // 20231215 22:50 ~ 23:31
     public static void main(String[] args) {
         int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
         int[] moves = {1,5,3,5,1,2,1,4};
