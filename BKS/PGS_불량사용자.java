db632309006454fbf351b6ffd064a56f919b05c3
kyoungsungBack <5165776@naver.com>
2023-12-27 16:29:25 +0900

    20231227 / 개인풀이 / [PGS] 불량사용자

--- BKS/PGS_불량사용자.java
+++ BKS/PGS_불량사용자.java
@@ -3,6 +3,7 @@ package BKS;
 import java.util.HashSet;
 
 public class PGS_불량사용자 {
+    // 20231227 09:11 ~ 09:23
     static String[] userIds;
     static String[] bannedIds;
     static HashSet<HashSet<String>> list;
@@ -19,23 +20,22 @@ public class PGS_불량사용자 {
 
         System.out.println(list.size());
     }
+
     public static void dfs(HashSet<String> set, int depth) {
-        // 집합이 생성되면 set에 넣기
+        // list는 만들어진 집합의 중복을 포함하지 않아야 하므로 set 선언
         if (depth == bannedIds.length) {
             list.add(set);
             return;
         }
 
-        // 만들 수 있는 집합 만들기
-        for (int i = 0 ; i < userIds.length ; i++) {
-            // 집합에 현재 원소가 있는 경우 무시(중복허용 x)
-            if (set.contains(userIds[i])) {
-                continue;
-            }
+        for (int i = 0; i < userIds.length; i++) {
+            if (set.contains(userIds[i])) continue;
 
-            // 현재 아이디가 제재 아이디인지 확인
+            // 완전탐색을 한다.
             if (cheek(userIds[i], bannedIds[depth])) {
                 set.add(userIds[i]);
+                // 기존 set을 인자로 안넣는 이유는?
+                // -> 새로운 집합을 계속 만드므로
                 dfs(new HashSet<>(set), depth + 1);
                 set.remove(userIds[i]);
             }
@@ -43,24 +43,17 @@ public class PGS_불량사용자 {
     }
 
     public static boolean cheek(String str1, String str2) {
-        // 두 str의 길이가 다르면 매핑 오류
-        if (str1.length() != str2.length()) {
-            return false;
-        }
+        // 길이가 다르면 다른 글자이므로 false
+        if (str1.length() != str2.length()) return false;
 
-        // str2가 *이고 의 길이가 다르면 매핑 오류
+        // 길이 같고 *를 제외한 글자가 같으면 true
         boolean isCheek = true;
-        for (int i = 0 ; i < str2.length() ; i++) {
-            if (str2.charAt(i) == '*') {
-                continue;
-            }
-
-            if (str1.charAt(i) != str2.charAt(i)) {
+        for (int i = 0; i < str2.length(); i++) {
+            if (str2.charAt(i) != '*' && str1.charAt(i) != str2.charAt(i)) {
                 isCheek = false;
                 break;
             }
         }
-
         return isCheek;
     }
 }
