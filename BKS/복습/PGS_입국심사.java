29c22e889d07eb3c514770de5eca4965b377fb94
kyoungsungBack <5165776@naver.com>
2024-01-10 17:14:38 +0900

    20240110 / 12주차 / [PGS] 입국심사 복습

--- BKS/복습/PGS_입국심사.java
+++ BKS/복습/PGS_입국심사.java
@@ -1,31 +1,31 @@
 package BKS.복습;
 
 public class PGS_입국심사 {
+    // 20240110 15:46 ~ 51 16:06 ~ 20
     public static void main(String[] args) {
         int n = 6;
         int[] times = {7, 10};
         long answer = 0;
-        int len = times.length;
-
         long left = 1;
-        long right = (long) times[len - 1] * n;
-        long mid = 0;
+        long right = (long)times[times.length - 1] * n;
+        long mid = 0; // 시간값
         long mok = 0;
 
-        // 1. 몫을 기준으로 모두 분배가 되는 시점을 찾아간다,
-        while (left <= right) {
-            mok = 0;
-            mid = (left + right) / 2;
+        // 0. time배열은 int이므로 계산 시에 long으로 형변환 해준다.
 
-            // 2. 현재 시간으로 몇 명이 입국심사를 완료할 수 있는지 확인
+        // 1. 가장 최소의 시간 구할 때 까지 반복한다.
+        while (left <= right) {
+            mid = (left + right) / 2;
+            mok = 0;
+
             for (int i = 0 ; i < times.length ; i++) {
-                mok += (long) mid / times[i];
+                mok +=  mid / (long)times[i];
             }
 
-            // 3. 입국심사 인원을 n과 비교
-            if (mok < n) {  // 3.1 입국심사를 못받은 사람이 있는 경우
+            // 2. 심사 받을 인원이 n일 때의 최소값 mid를 찾는다.
+            if (mok < n) {
                 left = mid + 1;
-            } else {        // 3.2 입국심사를 받을 인원을 초과한 경우
+            } else {
                 right = mid - 1;
                 answer = mid;
             }
