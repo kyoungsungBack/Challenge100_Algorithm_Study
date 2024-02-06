602c2729904d8d208c544bbf13a370cd9b8e439b
kyoungsungBack <5165776@naver.com>
2024-02-06 18:43:28 +0900

    20240206 / 개인풀이 / [PGS] 땅따먹기

--- BKS/PGS_땅따먹기.java
+++ BKS/PGS_땅따먹기.java
@@ -8,8 +8,27 @@ public class PGS_땅따먹기 {
         int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
         answer = 0;
         N = land.length;
+        /* 풀이1. dfs 시간초과 */
+        // sol(0, 0, -1, land);
 
-        sol(0, 0, -1, land);
+        /* 풀이2. DP */
+        int[][] dp = new int[N][4];
+        dp[0] = land[0];
+
+        for (int i = 1 ; i < N ; i++) {
+            for (int j = 0 ; j < 4 ; j++) {
+                for (int k = 0 ; k < 4 ; k++) {
+                    if (j == k) continue;
+
+                    dp[i][j] = Math.max(dp[i][j], land[i][j] + dp[i - 1][k]);
+                }
+            }
+        }
+
+        for (int i = 0 ; i < 4 ; i++) {
+            answer = Math.max(answer, dp[N - 1][i]);
+        }
+
         System.out.println(answer);
     }
 
