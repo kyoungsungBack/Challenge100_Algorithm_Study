0c955506ad74211d4ce949285dcb9ab7e34844ac
kyoungsungBack <5165776@naver.com>
2024-04-02 00:18:05 +0900

    20240401 / 개인풀이 / [PGS] 시소짝꿍

--- BKS/PGS_시소짝꿍.java
+++ BKS/PGS_시소짝꿍.java
@@ -1,43 +1,33 @@
 package BKS;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PGS_시소짝꿍 {
-
     public static void main(String[] args) {
         long answer = 0;
-        int[] weights = {100,180,360,100,270};
-        int N = weights.length;
+        int[] weights = {100, 180, 360, 100, 270};
+        // 1. 큰 수 중에 만들 수 있는 작은 수를 찾기 위해 오름차순 정렬한다.
+        Arrays.sort(weights);
+        Map<Double, Integer> map = new HashMap<>();
 
-        boolean[] DP = new boolean[N];
-        int[] weight2 = new int[N];
-        int[] weight3 = new int[N];
-        int[] weight4 = new int[N];
+        for (int w : weights) {
+            // 2. 정렬된 수를 하나씩 뽑으며 이전의 작은 값을 만들 수 있는 경우를 찾는다.
+            double a = w * 1.0;
+            double b = (w * 2.0) / 3.0;
+            double c = (w * 1.0) / 2.0;
+            double d = (w * 3.0) / 4.0;
 
-        for (int i = 0 ; i < N ; i++) {
-            weight2[i] = weights[i] * 2;
-            weight3[i] = weights[i] * 3;
-            weight4[i] = weights[i] * 4;
+            if (map.containsKey(a)) answer += map.get(a);
+            if (map.containsKey(b)) answer += map.get(b);
+            if (map.containsKey(c)) answer += map.get(c);
+            if (map.containsKey(d)) answer += map.get(d);
+
+            // 3. map에 작은 수를 하나씩 담는다.
+            //    이때, 동일한 숫자가 존재하고 동일하면 기존 개수에 1을 추가한다.
+            map.put((w * 1.0), map.getOrDefault((w * 1.0), 0) + 1);
         }
-
-        int num = 0;
-        for (int i = 0 ; i < N ; i++) {
-            for (int j = i + 1 ; j < N ; j++) {
-                if (DP[j]) continue;
-
-                // 1. 아직 방문을 안했고 곱한 값이 같으면 answer++ 한다.
-                for (int k = 2 ; k <= 4 ; k++ ) {
-                    num = weights[i] * k;
-                    if (num == weight2[j] || num == weight3[j] || num == weight4[j]) {
-                        answer++;
-                        DP[j] = true;
-                        break;
-                    }
-                }
-            }
-            Arrays.fill(DP, false);
-        }
-
         System.out.println(answer);
     }
 }
