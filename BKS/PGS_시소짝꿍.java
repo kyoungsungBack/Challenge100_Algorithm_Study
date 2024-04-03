98965a690c119aff554bf152215bba4f94719650
kyoungsungBack <5165776@naver.com>
2024-04-03 19:07:47 +0900

    20240403 / 개인풀이 / [PGS] 시소짝꿍 복습

--- BKS/PGS_시소짝꿍.java
+++ BKS/PGS_시소짝꿍.java
@@ -6,27 +6,30 @@ import java.util.Map;
 
 public class PGS_시소짝꿍 {
     // 20240402 19:10 ~ 19:35 (복습)
+    // 20240403 18:53 ~ 19:06 (복습)
     public static void main(String[] args) {
         long answer = 0;
         int[] weights = {100, 180, 360, 100, 270};
-        // 1. weights의 작은 수 부터 Map에 담기 위해 정렬한다.
+        // 0. 어쨌든 같은 거리에 있는 것만 확인되면 쌍으로 인정된다.
+
+        // 1. weights을 오름차순으로 정렬하여 작은 수들과 같은 값이 되는지 확인한다.
         Arrays.sort(weights);
+
+        // 2. 나누었을 때 소수가 될 수 있으므로 Double, Integer 타입으로 선언한다.
         Map<Double, Integer> map = new HashMap<>();
 
-        // 2. weights의 무게를 담고 2,3,4배로 줄일 수 있는 수를 탐색한다.
         for (int w : weights) {
-            double a = w * 1.0; // 2.1. 원래 수를 담는다.
-            double b = (w * 2.0) / 3; // 2.2.
-            double c = (w * 1.0) / 2;
-            double d = (w * 3.0) / 4;
+            // 3. 큰 수가 작아질 수 있는 경우를 모두 확인한다.
+            double a = w * 1.0;
+            double b = (w * 2.0) / 3.0;
+            double c = (w * 1.0) / 2.0;
+            double d = (w * 3.0) / 4.0;
 
             if (map.containsKey(a)) answer += map.get(a);
             if (map.containsKey(b)) answer += map.get(b);
             if (map.containsKey(c)) answer += map.get(c);
             if (map.containsKey(d)) answer += map.get(d);
 
-            // 3. weights의 하나씩 map에 등록하여 이후 숫자로 만들 수 있는지
-            //    확일할 때 사용한다.
             map.put(a, map.getOrDefault(a, 0) + 1);
         }
         System.out.println(answer);
