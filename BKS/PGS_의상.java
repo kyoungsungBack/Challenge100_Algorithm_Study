2f4f20ff4b01274dfbfe5cd7a4faa8c8c8c60649
kyoungsungBack <5165776@naver.com>
2024-04-20 23:10:35 +0900

    20240420 / 개인풀이 / [PGS] 의상 복습

--- BKS/PGS_의상.java
+++ BKS/PGS_의상.java
@@ -4,31 +4,27 @@ import java.util.HashMap;
 
 public class PGS_의상 {
     // 20240419 17:07 ~ 17:53 (못품) -> 해시에 담고 방정식 풀이로 접근
+    // 20240420 22:56 ~ 23:06 (복습)
     public static void main(String[] args) {
-        int answer = 0;
+        int answer = 1;
         String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"},{"green_turban", "headgear"}};
-        // 1. 착용부위를 key로 하고 중복 횟수를 value로 map에 담는다.
+        // 0. 착용부위를 key로 하고 중복 횟수를 value로 map에 담는다.
         HashMap<String, Integer> map = new HashMap<>();
 
-        // 2. getOrDefault를 통해 처음에 없던 값은 0 + 1이 되도록한다.
+        // 1. map에 각 착용부위의 의상 개수를 더 한다.
         for (int i = 0 ; i < clothes.length ; i++) {
+            // 1.1. getOrDefault을 통해 기존에 있는 키는 그때의 벨류 아니면 0을 반환한다.
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
 
-        // 3.1. 종류가 두 개 이상이면 아래와 같은 공식에 의해 경우의 수가 생성된다.
-        // 두 개 이상의 종류를 골라입는 경우의 수
-        // a + b + c + ab + ac + bc + abc = (a + 1)(b + 1)(c + 1) - 1
-        if (map.size() != 1) {
-            int num = 1;
-
-            for (String key : map.keySet()) {
-                num *= map.get(key) + 1;
-            }
-            answer += num - 1;
-        } // 3.2. 만약 종류가 하나이면 옷 종류만큼 경우의 수가 생성된다.
-        else {
-            answer = clothes.length;
+        // 2. map에서 방정식 공식에 따라 의상을 입을 수 있는 경우의 수를 계산한다.
+        for (String key : map.keySet()) {
+            answer *= (map.get(key) + 1);
         }
+        answer -= 1;
+
+        // 3. 만약 map의 개수가 1개일 경우 clothes길이 개수만큼 답이다.
+        if (map.size() == 1) answer = clothes.length;
 
         System.out.println(answer);
     }
