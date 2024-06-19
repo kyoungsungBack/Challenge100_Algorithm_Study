7977b0d82722bb3286521a880c3e0065309acb32
kyoungsungBack <5165776@naver.com>
2024-06-19 23:57:19 +0900

    20240619 / 개인풀이 / [PGS] 기능개발 복습

--- BKS/복습/PGS_기능개발.java
+++ BKS/복습/PGS_기능개발.java
@@ -7,41 +7,39 @@ import java.util.Queue;
 
 public class PGS_기능개발 {
     // 20231203 21:13 ~ 21:34
+    // 20240619 22:18 ~ 22:52
     public static void main(String[] args) {
         int[] progresses = {93, 30, 55};
         int[] speeds = {1, 30, 5};
         ArrayList<Integer> list = new ArrayList<>();
         Queue<Integer> que = new LinkedList<>();
 
-        int day = 0;
-        for (int i = 0; i < progresses.length; i++) {
-            day = (100 - progresses[i]) / speeds[i];
-            if ((100 - progresses[i]) % speeds[i] == 0) {
-                que.add(day);
-            } else {
-                que.add(day + 1);
-            }
-
+        for (int i = 0 ; i < progresses.length ; i++) {
+            int num1 = 100 - progresses[i];
+            int num2 = num1 % speeds[i] == 0 ? num1 / speeds[i] : (num1 / speeds[i]) + 1;
+            que.offer(num2);
         }
 
-        // 남은시간이 100이 넘어가면 계속 poll해주고 그만큼 개수를 answer에 담는다.
+        int temp = que.peek();
+        int cnt = 0;
         while (!que.isEmpty()) {
-            int cnt = 1;
-            int cur = que.poll();
+            int cur = que.peek();
 
-            while (!que.isEmpty() && cur >= que.peek()) {
-                que.poll();
+            if (temp >= cur) {
                 cnt++;
+                que.poll();
+            } else {
+                list.add(cnt);
+                cnt = 0;
+                temp = que.peek();
             }
-
-            list.add(cnt);
         }
+        list.add(cnt);
 
         int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0 ; i < list.size() ; i++) {
             answer[i] = list.get(i);
         }
-
         System.out.println(Arrays.toString(answer));
     }
 }
