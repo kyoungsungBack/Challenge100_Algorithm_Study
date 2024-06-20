08073fc4a7061dfba30d604aaf928cb8e8d7e75f
kyoungsungBack <5165776@naver.com>
2024-06-20 22:53:11 +0900

    20240620 / 개인풀이 / [PGS] 프로세스 복습

--- BKS/복습/PGS_프로세스.java
+++ BKS/복습/PGS_프로세스.java
@@ -7,43 +7,43 @@ import java.util.Queue;
 
 public class PGS_프로세스 {
     // 20240304 17:25 ~ 18:01 21:00 ~ 21:17 (36분 + 17분 = 53분)
+    // 20240620 22:25 ~ 22:50
     public static void main(String[] args) {
         int[] priorities = {2, 1, 3, 2};
         int location = 2;
-        int answer = 1;
+        int answer = 0;
+
         Queue<int[]> que = new LinkedList<>();
+        int cnt = 0;
 
-        // 1.1. priorities를 내림차순으로 정렬한다.
-        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>(){
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2 - o1;
-            }
-        });
-
-        // 1.2. int[] 에는 int[0] 인덱스, int[1] 우선순위 를 세팅한다.
-        int idx = 0;
-        for (int priority : priorities) {
-            pq.offer(priority);
-            que.offer(new int[]{idx++, priority});
+        for (int i = 0 ; i < priorities.length ; i++) {
+            que.offer(new int[]{i, priorities[i]});
         }
 
-        // 2. que가 빌 때 까지 프로세스를 실행하고 원하는 위치의 프로세스 발견하면 종료한다.
-        while (!que.isEmpty()) {
+        while (true) {
             int[] cur = que.poll();
+            boolean isOut = true;
 
-            // 3.1. 현재 가장 큰 수이며, 원하는 위치이면 반복문 탈출한다.
-            if (cur[1] == pq.peek() && cur[0] == location) {
-                break;
-            } // 3.2. 현재 que 중에 가장 큰 수 이면 answer 1증가, pq poll한다.
-            else if (cur[1] ==  pq.peek() && cur[0] != location) {
-                answer++;
-                pq.poll();
-            } // 3.3. 나갈 순서도 아니도 작은 수이면 다시 que에 담는다.
-            else {
+            // 현재 본인보다 큰 수가 있으면 다시 들어간다.
+            for (int[] temp : que) {
+                if (cur[1] < temp[1]) {
+                    isOut = false;
+                    break;
+                }
+            }
+
+            // 현재 빠져나갈 차례고 찾으려는 순번이면 종료한다.
+            if (isOut) {
+                cnt++;
+                if (cur[0] == location) {
+                    answer = cnt;
+                    break;
+                }
+            } else {
                 que.offer(cur);
             }
         }
+
         System.out.println(answer);
     }
 }
