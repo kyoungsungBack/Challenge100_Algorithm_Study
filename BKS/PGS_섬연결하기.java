2485985927bef06ef76caf84805011fa7c216154
kyoungsungBack <5165776@naver.com>
2024-07-17 22:49:39 +0900

    20240717 / 개인풀이 / [PGS] 섬연결하기 복습

--- BKS/PGS_섬연결하기.java
+++ BKS/PGS_섬연결하기.java
@@ -3,11 +3,13 @@ package BKS;
 import java.util.Arrays;
 
 public class PGS_섬연결하기 {
+    // 20240717 22:28 ~ 22:47
     public static int[] parent;
 
     public static void main(String[] args) {
         int n = 4;
         int[][] costs = {{0,1,1},{0,2,2},{1,2,5},{1,3,1},{2,3,8}};
+        // 크루스칼 -> 최소의 비용으로 최단거리를 찾는 그리디 방식
 
         int answer = 0;
         parent = new int[n];
@@ -15,8 +17,11 @@ public class PGS_섬연결하기 {
             parent[i] = i;
         }
 
+        // 1. 비용 기준으로 오름차순 정렬
         Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
 
+        // 2. 현재 두 노드의 부모가 다르면 합친다.
+        //  -> 또한, 연결된 비용을 더한다. 최소신장트리가 완성되면 더이상 비용이 더해지지 않음
         for (int[] cost : costs) {
             if (find(cost[0]) != find(cost[1])) {
                 union(cost[0], cost[1]);
