6307a350b5b94c238babbf74a746c15fe824e4f1
kyoungsungBack <5165776@naver.com>
2024-08-11 16:14:11 +0900

    20240811 / 개인풀이 / [PGS] 숫자카드나누기 복습

--- BKS/복습/PGS_숫자카드나누기.java
+++ BKS/복습/PGS_숫자카드나누기.java
@@ -2,44 +2,41 @@ package BKS.복습;
 
 public class PGS_숫자카드나누기 {
     // 20240804 17:39 ~ 18:07
+    // 20240811 15:50 ~ 16:12
     public static void main(String[] args) {
         int[] arrayA = {14, 35, 119};
         int[] arrayB = {18, 30, 102};
         int answer = 0;
 
-        // 0. arrayA, arrayB 각 배열의 최대공약수를 찾는다.
-        int aNum = arrayA[0];
-        int bNum = arrayB[0];
+        // 0. A,B 배열의 최대공약수를 찾기 위한 변수 초기화.
+        int gcdA = arrayA[0];
+        int gcdB = arrayB[0];
 
-        // 1. arrayA의 최대공약수를 구한다.
+        // 1. 각 A,B 배열의 최대공약수를 찾는다.
         for (int i = 1 ; i < arrayA.length ; i++) {
-            aNum = gcd(aNum, arrayA[i]);
+            gcdA = gcd(gcdA, arrayA[i]);
+            gcdB = gcd(gcdB, arrayB[i]);
         }
 
-        // 2. arrayB의 최대공약수를 구한다.
-        for (int i = 1 ; i < arrayB.length ; i++) {
-            bNum = gcd(bNum, arrayB[i]);
-        }
-
-        // 3. arrayB의 모든 수를 aNum으로 나눈다.
-        for (int num : arrayB) {
-            if (num % aNum == 0) {
-                aNum = 0;
+        // 2.1. gcdA로 arrayB를 나눌 수 있는지 확인한다.
+        for (int numB : arrayB) {
+            if (numB % gcdA == 0) {
+                gcdA = 0;
                 break;
             }
         }
-        answer = aNum;
+        answer = gcdA;
 
-        // 4. arrayA의 모든 수를 bNum으로 나눈다.
-        for (int num : arrayA) {
-            if (num % bNum == 0) {
-                bNum = 0;
+        // 2.2. gcdB로 arrayA를 나눌 수 있는지 확인한다.
+        for (int numA : arrayA) {
+            if (numA % gcdB == 0) {
+                gcdB = 0;
                 break;
             }
         }
 
-        // 5. 조건1, 2 중 하나는 만족하는 최대 수를 answer에 대입한다.
-        answer = Math.max(answer, bNum);
+        // 3. 조건을 만족하는 최대의 수를 answer에 담는다.
+        answer = Math.max(answer, gcdB);
         System.out.println(answer);
     }
     public static int gcd(int a, int b) {
