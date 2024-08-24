69fc8380f90889c4f6384dc26269bf2d72678d31
kyoungsungBack <5165776@naver.com>
2024-08-24 15:36:10 +0900

    20240824 / 개인풀이 / [PGS] 순위 복습

--- BKS/PGS_순위.java
+++ BKS/PGS_순위.java
@@ -2,6 +2,7 @@ package BKS;
 
 public class PGS_순위 {
     // 20240821 21:35 ~ 22:35
+    // 20240824 15:10 ~ 15:33 (복습)
     public static void main(String[] args) {
         int n = 5;
         int[][] results = {{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}};
@@ -11,41 +12,38 @@ public class PGS_순위 {
         // 플로이드-워셜 알고리즘 적용
         // 이기면 1, 지면 2
 
-        // 1. 배열에 각 번호들의 승패를 기록한다.
+        // 1. 먼저, 정확하게 순위를 매길 수 있는 선수의 수를 구하는 법을 찾는다.
+        // -> n명이 참여할 때 (n - 1)의 경기 결과를 보유한 경우가 존재
         for (int[] result : results) {
-            int w = result[0];
-            int l = result[1];
-            map[w][l] = 1;
-            map[l][w] = 2;
+            int a = result[0];
+            int b = result[1];
+
+            map[a][b] = 1;
+            map[b][a] = 2;
         }
 
-        // 2. 세 점에 대해 항상 이기는 경우와 지는 경우를 기록한다.
-        for (int i = 1 ; i <= n ; i++) {
-            for (int j = 1 ; j <= n ; j++) {
-                for (int k = 1 ; k <= n ; k++) {
-                    if (map[i][j] == 1 && map[j][k] == 1) {
-                        map[i][k] = 1;
-                        map[k][i] = 2;
-                    }
-
-                    if (map[i][j] == 2 && map[j][k] == 2) {
-                        map[i][k] = 2;
-                        map[k][i] = 1;
+        // 2. 선수 A, B, C가 있을 때 A가 B를 이기고 B는 C를 이기면 A는 C를 이긴다
+        // -> 플로이드-워셜 알고리즘 적용(이기고 지는 경우 동일)
+        for (int k = 1 ; k <= n ; k++) {
+            for (int i = 1 ; i <= n ; i++) {
+                for (int j = 1 ; j <= n ; j++) {
+                    if (map[i][k] == 1 && map[k][j] == 1) {
+                        map[i][j] = 1;
+                        map[j][i] = 2;
                     }
                 }
             }
         }
 
-        // 3. 각 번호의 모든 경기 결과를 알 수 있는 경우만 등수를 알 수 있다.
+
+        // 3. 경기 결과를 바탕으로 (n-1)개의 결과를 보유한 선수의 수를 반환한다.
         for (int i = 1 ; i <= n ; i++) {
             int cnt = 0;
-            for (int j = 1 ; j <= n ;j++) {
-                if (map[i][j] == 1 || map[i][j] == 2) {
-                    cnt++;
-                }
+            for (int j = 1 ; j <= n ; j++) {
+                if (i != j && map[i][j] != 0) cnt++;
             }
 
-            if (cnt == n - 1) answer++;
+            if (cnt == (n - 1)) answer++;
         }
 
         System.out.println(answer);
