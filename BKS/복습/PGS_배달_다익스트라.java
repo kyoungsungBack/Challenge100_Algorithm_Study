cbeac7941b1b816baf5a664b8427a4a516c07e7d
kyoungsungBack <5165776@naver.com>
2024-09-07 16:40:44 +0900

    20240907 / 개인풀이 / [PGS] 배달_다익스트라

--- BKS/복습/PGS_배달_다익스트라.java
+++ BKS/복습/PGS_배달_다익스트라.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class PGS_배달_다익스트라 {
-    static ArrayList<Node>[] list;
-    static int[] distance;
+    // 20240907 16:11 ~ 16:36
     public static void main(String[] args) {
         int N = 5;
         int[][] road = {{1,2,1}, {2,3,3},{5,2,2},{1,4,2},{5,3,1},{5,4,2}};
@@ -14,28 +13,48 @@ public class PGS_배달_다익스트라 {
         int answer = 0;
 
         // 0. 최단거리 배열을 만든다.
-        distance = new int[N + 1];
-        Arrays.fill(distance, 20000001);
+        int[] distance = new int[N + 1];
+        Arrays.fill(distance, 20000001); // 간선수 * 가중치
 
         // 1. 간선정보는 인접리스트에 저장한다.
-        list = new ArrayList[N + 1];
+        ArrayList<Node>[] list = new ArrayList[N + 1];
 
-        for (int i = 1 ; i <= N ; i++) {
+        for (int i = 0 ; i <= N ; i++) {
             list[i] = new ArrayList<>();
         }
 
         for (int[] arr : road) {
             int start = arr[0];
             int end = arr[1];
-            int value = arr[2];
+            int weight = arr[2];
 
-            list[start].add(new Node(end, value));
-            list[end].add(new Node(start, value));
+            list[start].add(new Node(end, weight));
+            list[end].add(new Node(start, weight));
         }
 
-        dijkstra(1);
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(1, 0));
+        distance[1] = 0;
 
-        for (int i = 1 ; i <= N ; i++) {
+        while(!pq.isEmpty()) {
+            Node cur = pq.poll();
+
+            // 현재 점까지 온 거리가 최단거리 배열의 값보다 크면 무시한다.
+            if (cur.weight > distance[cur.node] ) {
+                continue;
+            }
+
+            // 현재 점과 연결된 점으로 이동하며 최단거리를 갱신한다.
+            for (Node next : list[cur.node]) {
+                // 연결된 점의 최단거리와 현재점의 최단거리 + 연결점으로 이동하는 가중치를 비교한다.
+                if (distance[next.node] > distance[cur.node] + next.weight) {
+                    distance[next.node] = distance[cur.node] + next.weight;
+                    pq.offer(new Node(next.node, distance[next.node]));
+                }
+            }
+        }
+
+        for (int i = 1 ; i < distance.length ; i++) {
             if (distance[i] <= K) {
                 answer++;
             }
@@ -43,32 +62,8 @@ public class PGS_배달_다익스트라 {
 
         System.out.println(answer);
     }
-    public static void dijkstra(int start) {
-        // 각 노드의 가중치 기준 오름차순
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        distance[start] = 0;
-        pq.add(new Node(start, 0));
-
-        while (pq.isEmpty() != true) {
-            Node cur = pq.poll();
-
-            // 현재 노드 까지의 최단거리와 최단거리 배열의 값을 비교
-            // 이때, 현재 배열에 저장된 가중치가 더 적으면 무시한다.
-            if (cur.weight > distance[cur.node]) {
-                continue;
-            }
-
-            for (Node next : list[cur.node]) {
-                // 연결된 점을 이동했을 때 더 최단거리이면 갱신한다.
-                if (distance[next.node] > distance[cur.node] + next.weight) {
-                    distance[next.node] = distance[cur.node] + next.weight;
-
-                    pq.offer(new Node(next.node, distance[next.node]));
-                }
-            }
-        }
-    }
 }
+
 class Node implements Comparable<Node>{
     int node;
     int weight;
