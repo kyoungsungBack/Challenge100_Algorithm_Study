855cd00f61c24981eeb06fc7a52bc4b7965cf32d
kyoungsungBack <5165776@naver.com>
2024-10-01 00:40:07 +0900

    20241001 / 개인풀이 / [PGS] 피로도

--- BKS/복습/PGS_피로도.java
+++ BKS/복습/PGS_피로도.java
@@ -1,27 +1,25 @@
 package BKS.복습;
 
 public class PGS_피로도 {
+    // 20241001 00:23 ~ 00:36
     static int answer;
 
     public static void main(String[] args) {
         int k = 80;
         int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
-        answer = 0;
-        dfs(0, k, new boolean[dungeons.length], dungeons);
+        answer = -1;
+        sol(0, k, dungeons, new boolean[dungeons.length]);
         System.out.println(answer);
     }
 
-    public static void dfs(int n, int cur, boolean[] visited, int[][] dungeons) {
-        if (n > answer) {
-            answer = Math.max(answer, n);
-            System.out.println(cur + " " + answer);
-        }
+    public static void sol(int cnt, int k, int[][] dungeons, boolean[] isVisited) {
+        answer = Math.max(answer, cnt);
 
-        for (int i = 0; i < dungeons.length; i++) {
-            if (visited[i] == false && cur >= dungeons[i][0]) {
-                visited[i] = true;
-                dfs(n + 1, cur - dungeons[i][1], visited, dungeons);
-                visited[i] = false;
+        for (int i = 0 ; i < dungeons.length ; i++) {
+            if (!isVisited[i] && k >= dungeons[i][0]) {
+                isVisited[i] = true;
+                sol(cnt + 1, k - dungeons[i][1], dungeons, isVisited);
+                isVisited[i] = false;
             }
         }
     }
